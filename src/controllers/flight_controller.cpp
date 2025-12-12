ba7c2ced9e427b238189da3dcf382d5e64bb8e4f
Linus Lundgren <linus.c.lundgren@gmail.com>
2025-12-12 23:09:47 +0100

    Normalized movement speed.

--- src/controllers/flight_controller.cpp
+++ src/controllers/flight_controller.cpp
@@ -11,7 +11,8 @@ FlightController::FlightController(GLFWwindow* window, Camera* camera)
         GLFW_KEY_D,
         GLFW_KEY_ESCAPE,
         GLFW_KEY_SPACE,
-        GLFW_KEY_LEFT_CONTROL
+        GLFW_KEY_LEFT_CONTROL,
+        GLFW_KEY_LEFT_SHIFT
     };
 };
 
@@ -28,33 +29,38 @@ void FlightController::processKeyboardInput(std::map<int, int> &keyboardState)
     if (keyboardState[GLFW_KEY_W] == GLFW_PRESS)
     {
         if (keyboardState[GLFW_KEY_S] == GLFW_RELEASE)
-            localVelocity += glm::vec3(0.0f, 0.0f, -speed);
+            localVelocity += glm::vec3(0.0f, 0.0f, -1.0);
     }
     else if (keyboardState[GLFW_KEY_S] == GLFW_PRESS)
-        localVelocity += glm::vec3(0.0f, 0.0f, speed);
+        localVelocity += glm::vec3(0.0f, 0.0f, 1.0);
 
     // left/right
     if (keyboardState[GLFW_KEY_A] == GLFW_PRESS)
     {
         if (keyboardState[GLFW_KEY_D] == GLFW_RELEASE)
-            localVelocity += glm::vec3(-speed, 0.0f, 0.0f);
+            localVelocity += glm::vec3(-1.0, 0.0f, 0.0f);
     }
     else if (keyboardState[GLFW_KEY_D] == GLFW_PRESS)
-        localVelocity += glm::vec3(speed, 0.0f, 0.0f);
+        localVelocity += glm::vec3(1.0, 0.0f, 0.0f);
     
     // up/down
     if (keyboardState[GLFW_KEY_SPACE] == GLFW_PRESS)
     {
         if (keyboardState[GLFW_KEY_LEFT_CONTROL] == GLFW_RELEASE)
-            localVelocity += glm::vec3(0.0f, speed, 0.0f);
+            localVelocity += glm::vec3(0.0f, 1.0, 0.0f);
     }
     else if (keyboardState[GLFW_KEY_LEFT_CONTROL] == GLFW_PRESS)
-        localVelocity += glm::vec3(0.0f, -speed, 0.0f);
+        localVelocity += glm::vec3(0.0f, -1.0, 0.0f);
+
+    // normalize velocity vector and set to movement speed, if velocity is nonzero
+    if (glm::length(localVelocity) > 0.0001f)
+        localVelocity = glm::normalize(localVelocity) * (keyboardState[GLFW_KEY_LEFT_SHIFT] == GLFW_RELEASE ? speed : speed * 2);
 
     // set velocity in local basis and update position based on velocity
     camera->setLocalVelocity(localVelocity);
     camera->updatePosition();
 
+    // cursor capture
     if (!escapeHold && keyboardState[GLFW_KEY_ESCAPE] == GLFW_PRESS)
     {
         glfwSetInputMode(window, GLFW_CURSOR, freeCursor ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED);
